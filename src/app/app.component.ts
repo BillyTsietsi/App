61d83bcd55a32ec16c009dbd760676d2f6739e57
Billy <tb.mokolokoto@gmail.com>
2019-06-12 11:38:49 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  list = [];
+  newList:string ='';
+  editList:string ='';
+ 
+
+  addList(){
+    
+    this.list.push(this.newList)
+    this.newList=null;
+  }
+
+  delete(display) {
+    let index = this.list.indexOf(display)   
+    this.list.splice(index,1) 
+  
+      }
+
+      edit(display) {
+
+
+        let index = this.list.indexOf(display) 
+        let answer = prompt("Edit Task",display) 
+        this.list[index] = answer
+        
+        if (answer ==null || answer =="") {
+        
+          this.list[index] = display
+        }
+        
+      }
+
+      
+
+
+     
 }
+
+
